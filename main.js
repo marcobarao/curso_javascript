80fc738c269089a613cfddecfe6c7f431746897d
Marco Barão <marco.barao@outlook.com>
2018-10-20 16:56:22 -0300

    Utilizando Axios

--- main.js
+++ main.js
@@ -1,23 +1,5 @@
-var minhaPromisse = function() {
-  return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", "https://api.github.com/users/marcobarao");
-    xhr.send(null);
-
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          resolve(JSON.parse(xhr.responseText));
-        } else {
-          reject("Erro na requisição");
-        }
-      }
-    };
-  });
-};
-
-minhaPromisse()
+axios
+  .get("https://api.github.com/users/marcobarao")
   .then(function(response) {
     console.log(response);
   })
